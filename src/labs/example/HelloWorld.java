4989049f03d5ab8dd0e2f1a599d6dfeadd41d2ef
Charles Swain <swainch559@mail.sandhills.edu>
2026-01-21 11:48:55 -0500

    Initial commit: HelloWorld Java program

---
+++ src/labs/example/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+@author Charles Swain
+@date June 10, 2024
+@purpose This program prints "Hello, World!" to the console.
+ */
+package labs.example;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
