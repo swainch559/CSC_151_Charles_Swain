af1f8e34cf2e9289a7caa5d32f02db8959cb33c9
Charles Swain <swainch559@mail.sandhills.edu>
2026-01-28 10:41:11 -0500

    Refactor: Move HelloWorld to hello package and add gitignore

--- src/labs/example/hello/HelloWorld.java
+++ src/labs/example/hello/HelloWorld.java
@@ -3,7 +3,7 @@
 @date June 10, 2024
 @purpose This program prints "Hello, World!" to the console.
  */
-package labs.example;
+package labs.example.hello;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
